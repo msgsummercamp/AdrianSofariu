4b622d4858f4c1fed48aea87ba3607cd851512b9
AdrianSofariu <adrianradusofariu@gmail.com>
2025-07-23 11:39:01 +0300

    feat: angular app with navbar example

--- FELearning/my-app/src/app/app.component.ts
+++ FELearning/my-app/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-app';
