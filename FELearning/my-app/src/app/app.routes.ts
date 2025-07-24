ae77ce850247e07feac3be72e1366c3bd99d3080
AdrianSofariu <adrianradusofariu@gmail.com>
2025-07-24 14:23:42 +0300

    feat: added new routes and active route styling

--- FELearning/my-app/src/app/app.routes.ts
+++ FELearning/my-app/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
