2700c98aa905753e09776c9cbba46d709fc32ba7
AdrianSofariu <adrianradusofariu@gmail.com>
2025-07-25 09:31:13 +0300

    refactor: moved pipe example in about component

--- FELearning/my-app/src/app/about/about.component.ts
+++ FELearning/my-app/src/app/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TimesfivePipe } from '../pipes/timesfive.pipe';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [TimesfivePipe],
   templateUrl: './about.component.html',
+  providers: [TimesfivePipe],
 })
 export class AboutComponent {}
