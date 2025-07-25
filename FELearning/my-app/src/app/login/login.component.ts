e1e7af00f884a15193f355363e97d268d9530e34
AdrianSofariu <adrianradusofariu@gmail.com>
2025-07-25 10:08:31 +0300

    feat: initial login form created

--- FELearning/my-app/src/app/login/login.component.ts
+++ FELearning/my-app/src/app/login/login.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+type LoginForm = {
+  username: FormControl<string>;
+  email: FormControl<string>;
+  password: FormControl<string>;
+};
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private readonly _formBuilder = inject(NonNullableFormBuilder);
+
+  protected readonly loginFormGroup = this._formBuilder.group<LoginForm>({
+    username: this._formBuilder.control('', Validators.minLength(5)),
+    email: this._formBuilder.control('', Validators.email),
+    password: this._formBuilder.control('', Validators.minLength(5)),
+  });
+
+  protected onFormSubmit(): void {
+    if (this.loginFormGroup.valid) {
+      console.log('Raw values: ', this.loginFormGroup.getRawValue());
+    }
+  }
+}
