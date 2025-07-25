39683d38164b3fa219615c3e8dc1e28af0cbafb2
AdrianSofariu <adrianradusofariu@gmail.com>
2025-07-25 15:03:06 +0300

    refactor: removed unused import

--- FELearning/my-app/src/app/features/login/login.component.ts
+++ FELearning/my-app/src/app/features/login/login.component.ts
@@ -6,7 +6,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../core/auth/services/auth.service';
-import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
